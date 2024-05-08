5dcf4829bc1eeb80db83490a806dfa748bb40c4f
Dale Salter <dalejohnsalter@gmail.com>
2024-05-08 16:33:55 -0400

    ADDED: Basic s3 write

--- database/src/persistence/mod.rs
+++ database/src/persistence/mod.rs
@@ -5,8 +5,8 @@ pub mod s3;
 //  - Implement result class
 //  - Append some bytes to an existing path (WAL)
 pub trait Persistence {
-    fn write_blob(&self, path: &str, bytes: &[u8]) -> ();
-    fn read_blob(&self, path: &str) -> Result<Vec<u8>, ()>;
+    fn write_blob(&self, path: String, bytes: Vec<u8>) -> ();
+    fn read_blob(&self, path: String) -> Result<Vec<u8>, ()>;
     fn init(&self);
     fn reset(&self);
 }
