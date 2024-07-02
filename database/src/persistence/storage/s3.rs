5def48d01713388f7d1f167aad22f834a06c7a1a
Dale Salter <dalejohnsalter@gmail.com>
2024-07-01 21:50:09 -0400

    ADDED: S3 backend

--- database/src/persistence/storage/s3.rs
+++ database/src/persistence/storage/s3.rs
@@ -1,6 +1,7 @@
 use std::{path::PathBuf, sync::Arc, thread};
 
 use aws_sdk_s3::{primitives::ByteStream, Client};
+use chrono::Utc;
 use tokio::{
     runtime::Builder,
     sync::mpsc::{self, Sender},
@@ -23,14 +24,23 @@ struct ReadFileRequest {
     sender: oneshot::Sender<Vec<u8>>,
 }
 
+struct TransactionWriteRequest {
+    bytes: Vec<u8>,
+    sender: oneshot::Sender<()>,
+}
+
 enum S3Action {
     WriteBlob(WriteFileRequest),
     ReadBlob(ReadFileRequest),
     Reset(ResetFileRequest),
+    TransactionWrite(TransactionWriteRequest),
+    TransactionFlush(oneshot::Sender<()>),
+    TransactionLoad(oneshot::Sender<String>),
 }
 
 // TODO: Make configurable
 const S3_BUCKET: &str = "dalesalter-test-bucket";
+const TRANSACTION_LOG_PATH: &str = "transaction_log";
 
 // TODO:
 //  - Add error handling (_ + unwrap)
@@ -99,6 +109,93 @@ async fn handle_task(base_path: PathBuf, client: Arc<Client>, s3_action: S3Actio
             let bytes = response.body.collect().await.unwrap().into_bytes().to_vec();
 
             let _ = file_request.sender.send(bytes).unwrap();
+        }
+        S3Action::TransactionWrite(request) => {
+            let file_path = base_path
+                .join(TRANSACTION_LOG_PATH)
+                .join(Utc::now().to_rfc3339());
+
+            let req = client
+                .put_object()
+                .bucket(S3_BUCKET)
+                .key(file_path.to_str().unwrap())
+                .body(ByteStream::from(request.bytes));
+
+            let _ = req.send().await.unwrap();
+
+            let _ = request.sender.send(()).unwrap();
+        }
+        S3Action::TransactionFlush(r) => {
+            let transactions_folder = base_path.join(TRANSACTION_LOG_PATH);
+
+            // TODO: Make this dry w/ other methods
+            let mut response = client
+                .list_objects_v2()
+                .prefix(transactions_folder.to_str().unwrap())
+                .bucket(S3_BUCKET)
+                .max_keys(10)
+                .into_paginator()
+                .send();
+
+            while let Some(result) = response.next().await {
+                match result {
+                    Ok(output) => {
+                        for object in output.contents() {
+                            client
+                                .delete_object()
+                                .bucket(S3_BUCKET)
+                                .key(object.key().unwrap())
+                                .send()
+                                .await
+                                .unwrap();
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("{err:?}")
+                    }
+                }
+            }
+
+            let _ = r.send(()).unwrap();
+        }
+        S3Action::TransactionLoad(request) => {
+            let transactions_folder = base_path.join(TRANSACTION_LOG_PATH);
+
+            // TODO: Make this dry w/ other methods
+            let mut response = client
+                .list_objects_v2()
+                .prefix(transactions_folder.to_str().unwrap())
+                .bucket(S3_BUCKET)
+                .max_keys(10)
+                .into_paginator()
+                .send();
+
+            let mut contents: String = String::new();
+
+            while let Some(result) = response.next().await {
+                match result {
+                    Ok(output) => {
+                        for object in output.contents() {
+                            let result = client
+                                .get_object()
+                                .bucket(S3_BUCKET)
+                                .key(object.key().unwrap())
+                                .send()
+                                .await
+                                .unwrap();
+
+                            let result_bytes = result.body.collect().await.unwrap().into_bytes();
+
+                            contents.push_str(std::str::from_utf8(&result_bytes).unwrap());
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("{err:?}")
+                    }
+                }
+            }
+
+            let _ = request.send(contents).unwrap();
         }
     }
 }
@@ -181,18 +278,40 @@ impl Storage for S3Storage {
     }
 
     fn transaction_write(&mut self, transaction: &[u8]) -> () {
-        todo!()
+        let (sender, receiver) = oneshot::channel::<()>();
+
+        // TODO: Externalize transaction log
+        self.s3_action_sender
+            .blocking_send(S3Action::TransactionWrite(TransactionWriteRequest {
+                bytes: transaction.to_vec(),
+                sender: sender,
+            }))
+            .unwrap();
+
+        let _ = receiver.recv();
+    }
+
+    fn transaction_load(&mut self) -> String {
+        let (sender, receiver) = oneshot::channel::<String>();
+
+        self.s3_action_sender
+            .blocking_send(S3Action::TransactionLoad(sender))
+            .unwrap();
+
+        receiver.recv().unwrap()
+    }
+
+    fn transaction_flush(&mut self) -> () {
+        let (sender, receiver) = oneshot::channel::<()>();
+
+        self.s3_action_sender
+            .blocking_send(S3Action::TransactionFlush(sender))
+            .unwrap();
+
+        receiver.recv().unwrap()
     }
 
     fn transaction_sync(&self) -> () {
         // For s3 we do not need a disk sync
-    }
-
-    fn transaction_flush(&mut self) -> () {
-        todo!()
-    }
-
-    fn transaction_load(&mut self) -> String {
-        todo!()
     }
 }
