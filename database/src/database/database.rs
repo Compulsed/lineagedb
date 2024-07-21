8bf76c2e854fb551c2b4d43db8827b108c252709
Dale Salter <dalejohnsalter@gmail.com>
2024-07-21 13:36:42 -0400

    ADDED: Various changes

--- database/src/database/database.rs
+++ database/src/database/database.rs
@@ -1,7 +1,6 @@
 use std::{path::PathBuf, sync::Arc, thread, time::Instant};
 
 use num_format::{Locale, ToFormattedString};
-use tokio_postgres::row;
 use uuid::Uuid;
 
 use crate::{
@@ -196,7 +195,7 @@ impl Database {
                 .clone();
 
             log::info!(
-                "[Thread: {}. TxId: {}] Received request: {}",
+                "[Thread: {}. TxId: {}] ()Received request: {}",
                 thread_id,
                 transaction_timestamp,
                 command.log_format()
